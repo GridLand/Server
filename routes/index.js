519b943cac9183008251807ae7ac515134165356
CEB-HEF42NJ\차은별 <ceb0527@ajou.ac.kr>
2017-06-22 00:41:23 +0900

    get test

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//기록 저장
+router.get('/add/:id/:time', function (req, res) {
+    var id = req.params.id;
+    var time = req.params.time;
+
+    pool.getConnection(function (err, conn) {
+        var sqlForAddRecord= 'INSERT INTO records(player, time, date) VALUES (?, ?,NOW())';
+        conn.query(sqlForAddRecord, [id, time], function (err, row) {
+            if (err) {
+                conn.release();
+                throw err;
+            } else {
+                res.json(row);
+                conn.release();
+            }
+        })
+    })
+});
+
 module.exports = router;
